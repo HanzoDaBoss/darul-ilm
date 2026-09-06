f108ebcd8f596c7f76a8f061d361e1e0d8512412
HanzoDaBoss <hanif.uddz@gmail.com>
2026-09-06 19:32:40 +0100

    Added meta tag for google ownership verification

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -78,6 +78,10 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     meta: [
       { charSet: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        name: "google-site-verification",
+        content: "hSMOgJTmT-gvqqvjP7Er_ybGgKV9Nk4Q12j0HfvN0fM",
+      },
       { title: "Darul-ilm Kent | Maktab & Madrasa in Kent" },
       {
         name: "description",
